086a67fffbc963d9227176f0908a7d09d754268b
terrantsh <tanshanhe@foxmail.com>
2020-04-15 22:19:45 +0800

    完成变量，输入输出掌握

--- hello.py
+++ hello.py
@@ -33,6 +33,16 @@ print("My age is %d \r\n" %age)
 print("www","baidu","com", sep=".")
 '''
 
+'''
 password = input("password:")
 print("The password is ",password)
 print(type(password))
+print("the number is %s" %password)
+'''
+a = int("123")
+print(type(a))
+b = a + 100
+print(b)
+
+c = int(input("input:"))
+print("input number: %d" %c)
