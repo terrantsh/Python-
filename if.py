496b923fb5b6179979e96f895f85fcfd7e2450d9
terrantsh <tanshanhe@foxmail.com>
2020-04-16 18:56:36 +0800

    完成IF语句学习，完成猜拳

--- if.py
+++ if.py
@@ -4,6 +4,7 @@
 #@File      : if.py
 #@Software  : PyCharm
 
+'''
 if True:
     print("True")
 else :
@@ -19,3 +20,52 @@ if 0:
   print("True") # IndentationError: unindent does not match any outer indentation level
 else:
   print("False")
+'''
+
+'''
+score = 77
+
+if score >= 90:
+    print("A")
+elif score < 90 and score >= 60:
+    print("B")
+else :
+    print("C")
+'''
+
+'''
+xingbie = 1
+danshen = 1
+
+if xingbie == 1 :
+    print("Boy")
+    if danshen == 1:
+        print("introduce to you")
+    else:
+        print("introduce for me")
+else:
+    print("girl")
+    if danshen == 1:
+        print()
+    else:
+        print()
+'''
+
+import random
+
+x = random.randint(0,2)
+
+a = input("0,1,2:")
+
+if a == "0" or a == "1" or a == "2":
+    a = int(a)
+    if a - x == 0:
+        print("equal")
+    elif a - x == -1 or a - x == 2:
+        print("lose")
+    else:
+        print("win")
+else:
+    print("Wrong input")
+
+print("random:%d"%x)
