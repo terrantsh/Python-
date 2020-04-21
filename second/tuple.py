ea5463aab178b191422ed38e706e3d3338194cca
terrantsh <tanshanhe@foxmail.com>
2020-04-21 08:14:17 +0800

    更新元组的学习

---
+++ second/tuple.py
@@ -0,0 +1,24 @@
+#-*- coding = utf-8 -*-
+#@Time      : 2020/4/21 8:08
+#@Author    : Shanhe.Tan
+#@File      : tuple.py
+#@Software  : PyCharm
+
+'''
+tup1 = ()       #创建空的元组
+print(type(tup1))
+
+#tup2 = (50)    #<class 'int'>
+#tup2 = (50,)    #<class 'tuple'>
+
+tup2 = (50,60,70)   #<class 'tuple'>
+print(type(tup2))
+'''
+
+'''
+tup1 = ("abc","def",2000,2020)
+
+print(tup1[0])
+print(tup1[-1])     #访问最后一个元素
+print(tup1[1:5])    #左闭右开进行切片
+'''
